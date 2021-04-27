b1543d71fb47642fb9a4cce576a24249fb76fc88
lackatacker <salahazzouzi9@gmail.com>
2021-04-27 08:51:31 +0000

    t

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -4,10 +4,14 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 if (!key)
 return (NULL);
-const EC_GROUP *group = EC_KEY_get0_group(key);
-const EC_POINT *point = EC_KEY_get0_public_key(key);
-if (EC_POINT_point2octet(group, point, POINT_CONVERSION_UNCOMPRESSED,
-pub, EC_PUB_LEN, NULL))
-return (pub);
+const EC_GROUP *group;
+group = EC_KEY_get0_group(key);
+const EC_POINT *point;
+point = EC_KEY_get0_public_key(key);
+if (!group || !point)
 return (NULL);
+if (!EC_POINT_point2octet(group, point, POINT_CONVERSION_UNCOMPRESSED,
+pub, EC_PUB_LEN, NULL))
+return (NULL);
+return (pub);
 }
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -8,32 +8,18 @@
 #include <stdint.h>
 #include <string.h>
 #include <sys/stat.h>
-
 #define PUB_FILENAME "key_pub.pem"
 #define PRI_FILENAME "key.pem"
 #define EC_CURVE NID_secp256k1
-
-/* EC_KEY public key octet string length (using 256-bit curve) */
 #define EC_PUB_LEN 65
-/* Maximum signature octet string length (using 256-bit curve) */
 #define SIG_MAX_LEN 72
-
-/**
- * struct sig_s - EC Signature structure
- *
- * @sig: Signature buffer. The whole space may not be used
- * @len: Signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
- */
+#define PUB_FILENAME "key_pub.pem"
+#define PRI_FILENAME "key.pem"
 typedef struct sig_s
 {
-  /*
-   * @sig must stay first, so we can directly use the structure as
-   * an array of char
-   */
 uint8_t sig[SIG_MAX_LEN];
 uint8_t len;
 } sig_t;
-
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
@@ -45,5 +31,4 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 		 sig_t *sig);
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	      sig_t const *sig);
-
-#endif /* HBLK_CRYPTO_H */
+#endif
