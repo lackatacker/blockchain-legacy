9169e353f31fc26957145e517bb2a1ccb748a6ea
lackatacker <salahazzouzi9@gmail.com>
2021-04-27 09:00:55 +0000

    hi

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,17 +1,14 @@
 #include "hblk_crypto.h"
-
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-if (!key)
-return (NULL);
-const EC_GROUP *group;
-group = EC_KEY_get0_group(key);
 const EC_POINT *point;
+const EC_GROUP *group;
+if (!key || !pub)
+return (NULL);
 point = EC_KEY_get0_public_key(key);
-if (!group || !point)
-return (NULL);
-if (!EC_POINT_point2octet(group, point, POINT_CONVERSION_UNCOMPRESSED,
-pub, EC_PUB_LEN, NULL))
-return (NULL);
+group = EC_KEY_get0_group(key);
+if (EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED, pub,
+EC_PUB_LEN, NULL))
 return (pub);
+return (NULL);
 }
