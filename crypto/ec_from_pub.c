b9e5b68705726a42bce662dbfeb0ad47a8c7dda9
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-27 09:49:00 +0000

    Update ec_from_pub.c

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -8,11 +8,11 @@
  */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
-if(!pub)
-return (NULL);
 EC_KEY *key;
 EC_GROUP *group;
 EC_POINT *p;
+if(!pub)
+return (NULL);
 key = EC_KEY_new_by_curve_name(EC_CURVE);
 if(!key)
 return (NULL);
