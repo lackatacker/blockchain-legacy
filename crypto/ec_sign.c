a3016cd8faf0c32c38003aad9ec1f79d15639cd2
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 11:42:07 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -11,20 +11,11 @@
  * @msglen: msgs's length
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
-				 size_t msglen, sig_t *sig)
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+sig_t *sig)
 {
-	uint32_t len = 0;
-
-	if (!key || !msg || !msglen)
-		return NULL;
-
-	memset(sig->sig, 0, sizeof(sig->sig));
-	if (!ECDSA_sign(0, msg, (int)msglen, sig->sig, &len, (EC_KEY *)key))
-	{
-		sig->len = 0;
-		return NULL;
-	}
-	sig->len = (uint8_t)len;
-	return sig->sig;
+if (!key || !msg || !sig || !ECDSA_sign(EC_CURVE, msg, msglen, sig->sig,
+(unsigned int *)&(sig->len), (EC_KEY *)key))
+return (NULL);
+return (sig->sig);
 }
