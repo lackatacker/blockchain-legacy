faad2c63ce98ac2e28d361306a0d693c5cd44fcf
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 11:01:03 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,19 +1,17 @@
 #include "hblk_crypto.h"
+
 /**
- * ec_sign - Signs a given set of bytes, using a given EC_KEY public key
- *
- * test_ec_sign - Test the ec_sign function
- *
- * @key: Pointer to the EC Key pair to use to sign the message
- *
- * @msg: points to the msglen characters to verify the signature of
- * @sig: points to the signature to be checked
- * @msglen: msgs's length
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * ec_sign - Signs a given set of bytes, using a given EC_KEY
+ * @key: EC key pair
+ * @msg: message to be signed
+ * @msglen: length of that message
+ * @sig: pointer to  the signature
+ * Return: pointer to the signature on sucess NULL failure
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-sig_t *sig)
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+size_t msglen, sig_t *sig)
 {
+memset(sig->sig, 0, sizeof(sig->sig));
 if (key && msg)
 if (ECDSA_sign(0, msg, msglen, sig->sig,
 (unsigned int *) &sig->len, (EC_KEY *) key))
