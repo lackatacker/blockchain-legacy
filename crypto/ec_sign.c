2925efcee35d2ff4ed693241dadd7eb6c682b7aa
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 10:39:01 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,6 +1,6 @@
 #include "hblk_crypto.h"
 /**
- * @ec_sign: used to sign a given msg
+ * @ec_sign:  verifies the signature of a given set of bytes, using a given EC_KEY public key
  *
  * test_ec_sign - Test the ec_sign function
  *
