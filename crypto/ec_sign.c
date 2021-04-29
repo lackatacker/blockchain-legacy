3bcee2879c4c54b5f1449202a210590e8afb094d
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 11:04:05 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,30 +1,22 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - sign a message using an EC private key
- *
- * @key: pointer to the EC key pair containing the private key to be used
+ * ec_sign - Signs a given set of bytes, using a given EC_KEY private key
+ * @key: EC key pair already generated
  * @msg: message to be signed
- * @msglen: length of @message
- * @sig: address at which to store the signature
+ * @msglen: length of that message
+ * @sig: pointer to save the signature into
  *
- * Return: If key, msg, or sig is NULL, or if an error occurs, return NULL.
- * Otherwise, return a pointer to the buffer containing the signature.
- */
-uint8_t *ec_sign(
-	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+ * Return: pointer to the signature on sucess, NULL on failure
+ **/
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+sig_t *sig)
 {
-	unsigned int len = 0;
-
-	if (key && msg && msglen)
-	{
-		memset(sig->sig, 0, sizeof(sig->sig) / sizeof(*sig->sig));
-		if (ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key))
-		{
-			sig->len = len;
-			return (sig->sig);
-		}
-		sig->len = 0;
-	}
-	return (NULL);
+if (key == 0 || msg == 0 || sig == 0)
+return (NULL);
+if (ECDSA_sign(0, msg, msglen,
+sig->sig, (void *)&(sig->len),
+(void *)key) != 1)
+return (0);
+return (sig->sig);
 }
