e5f3eb80bce643d486ace1c7360db21c1922531a
lackatacker <salahazzouzi9@gmail.com>
2021-04-29 04:04:39 +0000

    adding sig verification

---
+++ crypto/ec_verify.c
@@ -0,0 +1,14 @@
+#include "hblk_crypto.h"
+/**
+ * test_ec_sign - Test the ec_sign function
+ *
+ * @key: Pointer to the EC Key pair to use to sign the message
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen, sig_t const* sig)
+{
+  if (!key | !msg | !sig | !ec_verify(key, msg, strlen((char*)str), &sig))
+    return (0);
+  return (1);
+}
