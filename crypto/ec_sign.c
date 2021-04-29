daf8c7b92ec1871fd319dabaeba180d20734e440
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 10:38:22 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,18 +1,20 @@
 #include "hblk_crypto.h"
 /**
  * @ec_sign: used to sign a given msg
- * 
+ *
  * test_ec_sign - Test the ec_sign function
- * 
+ *
  * @key: Pointer to the EC Key pair to use to sign the message
- * 
+ *
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+sig_t *sig)
 {
-if(!key || !msg)
+if (!key || !msg)
 return (NULL);
-if(ECDSA_sign(EC_CURVE, msg, msglen, sig->sig,(unsigned int *)&(sig->len), (EC_KEY *)key))
+if (ECDSA_sign(EC_CURVE, msg, msglen, sig->sig, (unsigned int *)&(sig->len)
+, (EC_KEY *)key))
 return (sig->sig);
 return (NULL);
 }
