512a159d7c30553eab2e4293ff8a2f29eaadd71e
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-29 12:02:17 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,21 +1,26 @@
 #include "hblk_crypto.h"
+
 /**
- * ec_sign - Signs a given set of bytes, using a given EC_KEY public key
+ * ec_sign - sign a set of bytes, using given private EC_KEY
+ * @key: pointer to EC_KEY struct containing private key to perform the signing
+ * @msg: pointer to characters to be signed
+ * @msglen: len of msg
+ * @sig: address to store signature
  *
- * test_ec_sign - Test the ec_sign function
- *
- * @key: Pointer to the EC Key pair to use to sign the message
- *
- * @msg: points to the msglen characters to verify the signature of
- * @sig: points to the signature to be checked
- * @msglen: msgs's length
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * Return: pointer to signature buffer on success, NULL on error
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-sig_t *sig)
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+	size_t msglen, sig_t *sig)
 {
-if (!key || !msg || !sig || !ECDSA_sign(EC_CURVE, msg, msglen, sig->sig,
-(unsigned int *)&(sig->len), (EC_KEY *)key))
+if (!key || !msg || !msglen)
 return (NULL);
-return (sig->sig);
+bzero(sig->sig, sizeof(sig->sig));
+sig->len = 0;
+if (!ECDSA_sign(0, msg, msglen, sig->sig,
+		(unsigned int *)&sig->len, (EC_KEY *)key))
+{
+sig->len = 0;
+return (NULL);
+}
+return ((uint8_t *)sig->sig);
 }
