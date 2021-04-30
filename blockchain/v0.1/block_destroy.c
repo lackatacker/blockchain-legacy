69590ad072e4c44c1fd4ff37e8dd26c91ba702c9
lackatacker <salahazzouzi9@gmail.com>
2021-04-30 21:38:55 +0000

    destroy update

--- blockchain/v0.1/block_destroy.c
+++ blockchain/v0.1/block_destroy.c
@@ -1,6 +1,6 @@
 #include "blockchain.h"
 /**
- * block_destroy - destroy a block.
+ * block_destroy - destroys a block.
  * @block: the block that is going to be destroyed.
  */
 void block_destroy(block_t *block)
