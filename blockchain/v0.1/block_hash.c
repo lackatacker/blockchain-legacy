fb8310ad0282e9796a716219a35489dc55661b2f
lackatacker <salahazzouzi9@gmail.com>
2021-04-30 21:53:03 +0000

    block_hash

---
+++ blockchain/v0.1/block_hash.c
@@ -0,0 +1,8 @@
+#include "blockchain.h"
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+  if(!block)
+    return (NULL);
+  SHA256((unsigned char *)(&block->data.buffer), block->data.len, hash_buf);
+  return (uint8_t *)(hash_buf);
+}
