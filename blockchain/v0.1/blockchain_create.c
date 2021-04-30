8b413ee8568807f0beec2f8ef1c2ecdf0b93b082
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 15:34:34 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -4,7 +4,7 @@ blockchain_t *blockchain_create(void)
   block_t *genesisBlock = malloc(sizeof(block_t)), *gen = BLOCK_GENESIS;
   blockchain_t *new_blockchain = malloc(sizeof(blockchain_t));
   new_blockchain->chain = llist_create(MT_SUPPORT_TRUE);
-  memcpy(&genesisBlock, gen, sizeof(GENESIS_BLOCK));
+  memcpy(&genesisBlock, gen, sizeof(block_t));
   if(!llist_add_node(new_blockchain->chain,(block_t *) &genesisBlock,ADD_NODE_FRONT))
     return (new_blockchain);
   return (NULL);
