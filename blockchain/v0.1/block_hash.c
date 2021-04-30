8a14f872107930c6c68048ffe581795cf65bf4e1
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 22:16:09 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -5,7 +5,7 @@
 uint8_t *block_hash(block_t const *block, uint8_t
 hash_buf[SHA256_DIGEST_LENGTH])
 {
-if(!block)
+if (!block)
 return (NULL);
 SHA256((unsigned char *)block, block->data.len+sizeof(block->info), hash_buf);
 return (uint8_t *)(hash_buf);
