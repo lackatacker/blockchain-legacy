237cf02ee660e2f8dae6f4d53c5d8bcba515b8ba
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 16:09:35 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,6 +1,6 @@
 #include "blockchain.h"
 /*
- * blockchain_create: creates a Blockchain structure, and initializes it.
+ * blockchain_create - creates a Blockchain structure, and initializes it.
  * return: 0 on success, and else on failure.
 */
 blockchain_t *blockchain_create(void)
