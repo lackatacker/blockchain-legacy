0149f81d75698c3fbffe72280666210d61bf96a2
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 18:40:52 +0000

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -4,11 +4,9 @@ block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len
 int mylen = (((BLOCKCHAIN_DATA_MAX) < (data_len)) ?
 (BLOCKCHAIN_DATA_MAX) : (data_len));
 block_t *block;
-if (!prev)
+if (!prev || !data)
 return (NULL);
 block = (block_t *) malloc( sizeof(block_t));
-if(!data)
-return (NULL);
 block->info.index = prev->info.index + 1;
 block->info.difficulty = 0;
 block->info.nonce = 0;
