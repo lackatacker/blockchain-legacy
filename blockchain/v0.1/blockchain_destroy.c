1b447a36c64579364b213a33955ab48953cd43eb
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 21:23:46 +0000

    Update blockchain_destroy.c

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,5 +1,10 @@
 #include "blockchain.h"
 void blockchain_destroy(blockchain_t *blockchain)
 {
-_blockchain_destroy(blockchain);
+if (!blockchain)
+		return;
+
+	llist_destroy(blockchain->chain, 1, (node_dtor_t)free);
+
+	free(blockchain);
 }
