7593ab97ca5243cc2430a02444cbf6d81044ef71
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-04-30 22:20:40 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,6 +1,8 @@
 #include "blockchain.h"
 /**
  * block_hash - hashes info + data
+ * @block: the block to be hashed.
+ * @hash_buf: the digest.
 */
 uint8_t *block_hash(block_t const *block, uint8_t
 hash_buf[SHA256_DIGEST_LENGTH])
