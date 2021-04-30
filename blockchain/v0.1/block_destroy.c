be6017b80bb7cf0fe8440f420fc24ff29c14da59
lackatacker <salahazzouzi9@gmail.com>
2021-04-30 21:10:47 +0000

    salah

--- blockchain/v0.1/block_destroy.c
+++ blockchain/v0.1/block_destroy.c
@@ -1,7 +1,8 @@
 #include "blockchain.h"
 /**
-* @block_destroy - frees the block.
-*/
+ * block_destroy - destroy a block.
+ * @block: the block that is going to be destroy.
+ */
 void block_destroy(block_t *block)
 {
 free(block);
