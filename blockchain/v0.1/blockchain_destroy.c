4548d9f98546d9dc80a0693fe7a1f4a8fb0e119a
lackatacker <salahazzouzi9@gmail.com>
2021-04-30 21:29:19 +0000

    teest

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,10 +1,13 @@
 #include "blockchain.h"
+/**
+ * blockchain_destroy - destroys a blockchain if
+ * it isn't already pointing on NULL.
+ * @blockchain: the blockchain to be freed.
+ */
 void blockchain_destroy(blockchain_t *blockchain)
 {
 if (!blockchain)
-		return;
-
-	llist_destroy(blockchain->chain, 1, (node_dtor_t)free);
-
-	free(blockchain);
+return;
+llist_destroy(blockchain->chain, 1, (node_dtor_t)free);
+free(blockchain);
 }
