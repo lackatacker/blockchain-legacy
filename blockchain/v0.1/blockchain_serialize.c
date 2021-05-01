1349fd71bf914acaa6fb81982b7c50853b388665
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-01 11:50:36 +0000

    Create blockchain_serialize.c

---
+++ blockchain/v0.1/blockchain_serialize.c
@@ -0,0 +1,24 @@
+#include "blockchain.h"
+int blockchain_serialize(blockchain_t const *blockchain, char const *path)
+{
+FILE *f;
+block_t myblock;
+list_size = llist_size(blockchain->chain);
+if (!blockchain || !path)
+return (-1);
+if (!(f=fopen(path,"w+"));
+return -1;
+write(f, HBLK_MAGIC, 4);
+write(f, HBLK_VERSION, 3);
+write(f, &endian(), 1);
+write(f, &list_size, 4);
+for (i=0 ; i < size ; i++)
+{
+myblock = llist_get_node_at(blockchain->chain, i++);
+write(f, &(block->info), sizeof(block->info));
+write(f, &(block->data.len, sizeof(block->data.len)));
+write(f, &(block->hash, SHA256_DIGEST_LENGTH));
+}
+close (f);
+return (0);
+}
