fe38ae37a44a8adce0a5b2361f0f8378317e7e5e
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-01 12:31:55 +0000

    Update blockchain_serialize.c

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -5,14 +5,14 @@ uint8_t end = _get_endianness();
 int f, i;
 block_t *myblock;
 int32_t list_size = llist_size(blockchain->chain);
-if (!blockchain || !path || !(f = open(path, O_CREAT |
-O_TRUNC | S_IRUSR | S_IWUSR)))
+if (!blockchain || !path || !f = open(path, O_CREAT |
+O_TRUNC | S_IRUSR | S_IWUSR))
 return (-1);
 write(f, HBLK_MAGIC, 4);
 write(f, HBLK_VERSION, 3);
 write(f, &end, 1);
 write(f, &list_size, 4);
-for (i=0 ; i < list_size ; i++)
+for (i = 0 ; i < list_size ; i++)
 {
 myblock = llist_get_node_at(blockchain->chain, i++);
 if (write(f, &(myblock->info), sizeof(myblock->info))
