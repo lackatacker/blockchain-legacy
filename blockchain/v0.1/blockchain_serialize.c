1d463f4de807d2bb57efaffded0c2f300341853a
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-01 12:22:54 +0000

    Update blockchain_serialize.c

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -10,14 +10,14 @@ O_TRUNC | S_IRUSR | S_IWUSR)))
 return (-1);
 write(f, HBLK_MAGIC, 4);
 write(f, HBLK_VERSION, 3);
-write(f, end, 1);
+write(f, &end, 1);
 write(f, &list_size, 4);
 for (i=0 ; i < size ; i++)
 {
 myblock = llist_get_node_at(blockchain->chain, i++);
 if (write(f, &(myblock->info), sizeof(block->info)) != sizeof(block->info) ||
 write(f, &(myblock->data.len, sizeof(block->data.len))) != block->data.len ||
-write(f, &(myblock->hash, &end, SHA256_DIGEST_LENGTH)) != SHA256_DIGEST_LENGTH ||
+write(f, &(myblock->hash, SHA256_DIGEST_LENGTH)) != SHA256_DIGEST_LENGTH ||
 write(f, myblock->data.buffer, myblock->data.len) != myblock->data.len)
 return (-1);
 }
