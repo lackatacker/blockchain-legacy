0611b9f8fae1bf6ffa0f94c3c49a3600f6769a59
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-01 23:13:22 +0000

    Update block_destroy.c

--- blockchain/v0.1/block_destroy.c
+++ blockchain/v0.1/block_destroy.c
@@ -1,9 +1,9 @@
-#include "blockchain.h"
+ #include "blockchain.h"
 /**
- * block_destroy - frees a block
- * @block: pointer to block to free
-*/
+ * block_destroy - destroys a block. 
+ * @block: the block that is going to be destroyed.
+ */
 void block_destroy(block_t *block)
 {
-free (block);
+free(block);
 }
