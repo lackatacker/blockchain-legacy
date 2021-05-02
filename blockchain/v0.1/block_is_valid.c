b8d82189116e4cc3aa28d46e94f6cfdfe50e3a20
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-02 17:05:49 +0000

    Update block_is_valid.c

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -9,7 +9,7 @@
 int is_genesis(block_t const *block)
 {
 blockchain_t *blockchain = blockchain_create();
-if (!blockchain)
+if (!blockchain || )
 return (-1);
 if (memcmp(block, llist_get_head(blockchain->chain), sizeof(*block)) != 0)
 {
@@ -23,19 +23,19 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 uint8_t hash_buf[SHA256_DIGEST_LENGTH];
 if (!block || ((block->info.index == 0) && prev_block))
-return (50);
+return (-1);
 if (block->info.index == 0 && !is_genesis(block))
-return (50);
+return (-1);
 if (block->info.index != prev_block->info.index + 1)
-return (50);
+return (-1);
 if (!block_hash(prev_block, hash_buf) || memcmp(hash_buf,
 prev_block->hash, sizeof(block_t)))
-return (50);
+return (-1);
 if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
-return (50);
+return (-1);
 if (memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH))
-return (50);
+return (-1);
 if (block->data.len < BLOCKCHAIN_DATA_MAX)
 return (0);
-return (50);
+return (-1);
 }
