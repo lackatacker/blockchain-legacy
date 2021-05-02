4e308607be16b9d131c01434ebda3d8321144cd6
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-02 15:55:02 +0000

    Update block_mine.c

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -2,10 +2,8 @@
 void block_mine(block_t *block)
 {
 uint8_t hash_buff[SHA256_DIGEST_LENGTH];
-if(!block)
-return (NULL);
 if(!hash_matches_difficulty(block->hash, block->info.difficulty))
-while(!hash_matches_difficulty(buff_hash, block->info.difficulty))
+while(!hash_matches_difficulty(hash_buff, block->info.difficulty))
 {
 block->info.nonce++;
 block_hash(block, hash_buff);
