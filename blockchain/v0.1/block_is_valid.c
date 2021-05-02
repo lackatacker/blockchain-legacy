5ab9fe14cdfbad59f4d5cfb227056eed7370edf4
lackatacker <salahazzouzi9@gmail.com>
2021-05-02 15:24:48 +0000

    hey

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -7,21 +7,20 @@
 */
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
-block_t genesis = BLOCK_GENESIS_INIT;
 uint8_t hash_buf[SHA256_DIGEST_LENGTH];
 if (!block || (block->info.index == 0 && prev_block))
 return (50);
-if (!prev_block && memcmp(block, &genesis, sizeof(genesis)))
+ if (!prev_block && is_genesis(block))
 return (50);
-if (block->info.index == 1 && memcmp(prev_block, &genesis, sizeof(genesis)))
+if (block->info.index == 1 && is_genesis(prev_block))
 return (50);
-if (!block_hash(block_prev, hash_buf) || memcmp(hash_buf, prev_block->hash))
+ if (!block_hash(prev_block, hash_buf) || memcmp(hash_buf, prev_block->hash, sizeof(block_t)))
 return (50);
 if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
 return (50);
-if (!block_hash(block, hash_buf) || memcmp(hash_buf, block->hash))
+ if (!block_hash(block, hash_buf) || memcmp(hash_buf, block->hash, sizeof(block_t)))
 return (50);
-if (block->data.length > BLOCKCHAIN_DATA_MAX)
+if (block->data.len > BLOCKCHAIN_DATA_MAX)
 return (50);
 return (0);
 }
