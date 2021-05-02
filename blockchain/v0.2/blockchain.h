6e7e55050e28c388925d20f2e6a1bbbd2dd2451a
lackatacker <salahazzouzi9@gmail.com>
2021-05-02 13:07:24 +0000

    hey

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -52,8 +52,9 @@ int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 
 blockchain_t *blockchain_deserialize(char const *path);
 int block_is_valid(block_t const *block, block_t const *prev_block);
-
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
 uint8_t _get_endianness(void);
+
 void _swap_endian(void *p, size_t size);
 #define GENESIS_BLOCK { \
 	{ /* info */ \
