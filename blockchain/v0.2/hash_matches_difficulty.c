9ac3822e39c79af48916d5150c7506701b132b6e
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-02 13:10:49 +0000

    Update hash_matches_difficulty.c

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -2,17 +2,17 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
-  uint8_t i,m,d;
-  if(!hash)
-    return (0);
-  d = difficulty / 8;
-  m = difficulty % 8;
-  for (i=0 ; i < d ; i++)
-    {
-      if(hash[i] & 0)
-	return 0;
-    }
-  if(hash[d]>>(8-m) == 0)
-    return (1);
-  return (0);
+uint8_t i ,m ,d;
+if (!hash)
+return (0);
+d = difficulty / 8;
+m = difficulty % 8;
+for (i = 0 ; i < d ; i++)
+{
+if (hash[i] & 0)
+return (0);
+}
+if (hash[d] >> (8 - m) == 0)
+return (1);
+return (0);
 }
