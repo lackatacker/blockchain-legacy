045e3a5c02327461285a540e896053a448f5d89b
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-02 17:13:40 +0000

    Update block_is_valid.c

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -29,7 +29,7 @@ return (is_genesis(block));
 if (block->info.index != prev_block->info.index + 1)
 return (-1);
 if (!block_hash(prev_block, hash_buf) || memcmp(hash_buf,
-prev_block->hash, SHA256_DIGEST_LENGTH)))
+prev_block->hash, SHA256_DIGEST_LENGTH))
 return (-1);
 if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
 return (-1);
