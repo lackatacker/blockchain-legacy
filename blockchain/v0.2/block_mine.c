06c6564afe98f25ecc7d666d0891ccd4eb40fcec
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-02 15:51:10 +0000

    Update block_mine.c

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,5 +1,14 @@
 #include "blockchain.h"
 void block_mine(block_t *block)
 {
-uint8_t hash_buff[SHA256_DIGEST_LENGTH];  
+uint8_t hash_buff[SHA256_DIGEST_LENGTH];
+if(!block)
+return (NULL);
+if(!hash_matches_difficulty(block->hash, block->info.difficulty))
+while(!hash_matches_difficulty(buff_hash, block->info.difficulty))
+{
+block->info.nonce++;
+block_hash(block, hash_buff);
+}
+memcpy(block->hash, hash_buff, SHA256_DIGEST_LENGTH);
 }
