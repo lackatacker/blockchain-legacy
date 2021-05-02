fc6c8dfbb210372d21f41a9f7e82b7b8bba0c7c6
lackatacker <salahazzouzi9@gmail.com>
2021-05-02 12:58:26 +0000

    mining update

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,12 +1,18 @@
 #include "blockchain.h"
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-uint32_t difficulty);
+			    uint32_t difficulty)
 {
-uint8_t i;
-if (!hash)
-return (0);
-for (i=0; i<difficulty ; i++)
-if (hash[i])
-return (0);
-return (1); 
+  uint8_t i,m,d;
+  if(!hash)
+    return (0);
+  d = difficulty / 8;
+  m = difficulty % 8;
+  for (i=0 ; i < d ; i++)
+    {
+      if(hash[i] & 0)
+	return 0;
+    }
+  if(hash[d]>>(8-m) == 0)
+    return (1);
+  return (0);
 }
