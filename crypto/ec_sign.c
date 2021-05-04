abb89d80c6d6329361274a06600404b60b18b2d2
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-04 14:09:26 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -10,7 +10,7 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 sig_t *sig)
 {
-if (key || msg || sig || ECDSA_sign(EC_CURVE, msg, msglen, sig->sig, 
+if (key && msg && sig && ECDSA_sign(EC_CURVE, msg, msglen, sig->sig, 
 (unsigned int *)&(sig->len), (EC_KEY *)key))
 return (sig->sig);
 return (NULL);
