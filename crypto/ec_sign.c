c859a7aa5a8f3ee5795dcac6e610a481709bc6d9
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-04 14:36:57 +0000

    Update ec_sign.c

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -7,11 +7,20 @@
  * @msglen: msgs's length.
  * Return: NULL, or the sig upon success.
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-sig_t *sig)
 {
-if (key && msg && ECDSA_sign(EC_CURVE, msg, msglen, sig->sig,
-(unsigned int *)&(sig->len), (EC_KEY *)key))
-return (sig->sig);
-return (NULL);
+unsigned char md[SHA256_DIGEST_LENGTH];
+
+	if (!key || !msg || !sig)
+		return (NULL);
+	if (!EC_KEY_check_key(key))
+		return (NULL);
+	if (!SHA256(msg, msglen, md))
+		return (NULL);
+	sig->len = ECDSA_size(key);
+	if (!sig->len)
+		return (NULL);
+	if (!ECDSA_sign(EC_CURVE, md, SHA256_DIGEST_LENGTH, sig->sig,
+				(unsigned int *)&(sig->len), (EC_KEY *)key))
+		return (NULL);
+	return (sig->sig);
 }
