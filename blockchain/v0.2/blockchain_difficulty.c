4375cbb02b937b44de6911d7cecf441583fee283
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-04 15:14:14 +0000

    Update blockchain_difficulty.c

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -7,7 +7,7 @@
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
 uint8_t block_index;
-uint32_t elapsed_time, expected_elapsed_time;
+uint32_t et, eetime;
 block_t *tail_block = llist_get_tail(blockchain->chain);
 block_t *last_modified_block;
 if (tail_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0
@@ -15,12 +15,12 @@ if (tail_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0
 {
 block_index = llist_size(blockchain->chain) - DIFFICULTY_ADJUSTMENT_INTERVAL + 1;
 last_modified_block = llist_get_node_at(blockchain->chain, block_index);
-expected_elapsed_time = block_index *BLOCK_GENERATION_INTERVAL;
-elapsed_time = tail_block->info.timestamp -
+eet = block_index *BLOCK_GENERATION_INTERVAL;
+et = tail_block->info.timestamp -
 last_modified_block->info.timestamp;
-if (elapsed_time < expected_elapsed_time * 2)
+if (elapsed_time < eet * 2)
 tail_block->info.difficulty++;
-else if (elapsed_time > expected_elapsed_time * 2)
+else if (eet > eet * 2)
 tail_block->info.difficulty--;
 }
 return (tail_block->info.difficulty);
