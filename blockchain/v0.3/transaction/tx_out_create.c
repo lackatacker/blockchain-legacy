efd236b42f282ef8336c520a4fc2aee750b2c697
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-08 04:46:31 +0000

    Update tx_out_create.c

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -4,8 +4,8 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
   tx_out_t *tx_out = malloc(sizeof(tx_out_t));
 if (!amount || !pub)
   return (NULL);
-if (!memcpy(tx_out->amount, amount, sizeof(amount)) ||
-   !memcpy(tx_out->pub, pub, sizeof(pub)))
+if (!memcpy(&tx_out->amount, amount, sizeof(amount)) ||
+   !memcpy(&tx_out->pub, pub, sizeof(pub)))
   return (NULL);
 return (tx_out);
 }
