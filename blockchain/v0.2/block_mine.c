fdb20614762750278153b4dd5911e1cda6245721
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-10 02:49:50 +0000

    Update block_mine.c

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,16 +1,21 @@
 #include "blockchain.h"
 /**
- * block_mine - finds a hash that respects difficulty and mines the block.
- * @block: the block to mine.
-*/
+ * block_mine - mining by changing the nonce
+ * @block: the mined block
+ */
 void block_mine(block_t *block)
 {
-uint8_t hash_buff[SHA256_DIGEST_LENGTH];
-block_hash(block, hash_buff);
-while (!hash_matches_difficulty(hash_buff, block->info.difficulty))
+uint8_t blockHash[SHA256_DIGEST_LENGTH];
+block_hash(block, blockHash);
+if (!hash_matches_difficulty(blockHash, block->info.difficulty))
 {
+block->info.nonce = 0;
+do {
 block->info.nonce++;
-block_hash(block, hash_buff);
+block_hash(block, blockHash);
+} while (!hash_matches_difficulty(blockHash,
+block->info.difficulty));
+
+memcpy(block->hash, blockHash, SHA256_DIGEST_LENGTH);
 }
-memcpy(block->hash, hash_buff, SHA256_DIGEST_LENGTH);
 }
