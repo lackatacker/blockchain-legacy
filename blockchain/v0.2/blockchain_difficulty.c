2ce388b66b238d87b205dac8be383ae7cc959df6
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-14 11:35:12 +0000

    Update blockchain_difficulty.c

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -7,21 +7,21 @@
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
 uint8_t block_index;
-uint32_t et, eet;
+uint32_t et, eet, newDifficulty = 0;
 block_t *tail_block = llist_get_tail(blockchain->chain), *last;
 if (tail_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0
 && tail_block->info.index != 0)
 {
 block_index = llist_size(blockchain->chain) - DIFFICULTY_ADJUSTMENT_INTERVAL;
 last = llist_get_node_at(blockchain->chain, block_index + 1);
+newDifficulty = last->info.index;
 eet = DIFFICULTY_ADJUSTMENT_INTERVAL * BLOCK_GENERATION_INTERVAL;
 et = tail_block->info.timestamp - last->info.timestamp;
 if (et < (eet * 2))
-tail_block->info.difficulty++;
+newDifficulty++;
 else if (eet > (eet * 2))
-tail_block->info.difficulty--;
-else
-tail_block->info.difficulty = 0;
+newDifficulty--;
+return (newDifficulty);
 }
 return (tail_block->info.difficulty);
 }
