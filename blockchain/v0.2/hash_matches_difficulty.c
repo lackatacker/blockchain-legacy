1b30c4f715b5e9df1369141630a10c391b580a59
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-15 20:17:04 +0000

    Update hash_matches_difficulty.c

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 #include "blockchain.h"
-
-
-
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
 uint32_t cpt = 0;
-uint8_t i, bit;
+int i, bit;
 for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
 {
 for (bit = 7; bit >= 0; bit--)
 {
-if (hash[i] & (1 << bit))
-return (0);
+if (hash[i] & 1 << bit)
+break;
 else
 cpt++;
 }
 }
-if (cpt == difficulty)
+if (cpt >= difficulty)
 return (1);
+else
+return (0);
 }
