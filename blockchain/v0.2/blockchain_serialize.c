e351dffc63428a66c7c1a89165422b631ac9b5e9
lackatacker <80588946+lackatacker@users.noreply.github.com>
2021-05-15 11:30:24 +0000

    Update blockchain_serialize.c

--- blockchain/v0.2/blockchain_serialize.c
+++ blockchain/v0.2/blockchain_serialize.c
@@ -21,13 +21,10 @@ write(f, &list_size, 4);
 for (i = 0 ; i < list_size ; i++)
 {
 myblock = llist_get_node_at(blockchain->chain, i++);
-if (write(f, &(myblock->info), sizeof(myblock->info))
-!= sizeof(myblock->info) ||
-write(f, &(myblock->data.len), sizeof(myblock->data.len))
-!= myblock->data.len ||
-write(f, &(myblock->hash), SHA256_DIGEST_LENGTH) != SHA256_DIGEST_LENGTH ||
-write(f, myblock->data.buffer, myblock->data.len) != myblock->data.len)
-return (-1);
+write(f, &(myblock->info), sizeof(myblock->info));
+write(f, &(myblock->data.len), sizeof(myblock->data.len));
+write(f, &(myblock->hash), SHA256_DIGEST_LENGTH);
+write(f, myblock->data.buffer, myblock->data.len);
 }
 close(f);
 return (0);
